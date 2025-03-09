4673add511d095a693e7365a5d9d609aa0798ef3
prodevuz <abdurakhmon278@gmail.com>
2025-03-09 20:00:52 +0500

    Add placeholder for voice command screen and navigation

--- lib/blocs/app_bloc.dart
+++ lib/blocs/app_bloc.dart
@@ -2,19 +2,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Events
 abstract class AppEvent {}
+
 class AppStarted extends AppEvent {}
 
 // States
 abstract class AppState {}
+
 class AppInitial extends AppState {}
+
 class AppLoaded extends AppState {}
 
 // Bloc
 class AppBloc extends Bloc<AppEvent, AppState> {
-  AppBloc(): super(AppInitial()) {
+  AppBloc() : super(AppInitial()) {
     on<AppStarted>((event, emit) async {
-      await Future.delayed(Duration(seconds: 2)); // Simulate loading TODO: Remove this line
+      await Future.delayed(
+        Duration(seconds: 2),
+      ); // Simulate loading TODO: Remove this line
       emit(AppLoaded());
     });
+
+    add(AppStarted());
   }
 }
